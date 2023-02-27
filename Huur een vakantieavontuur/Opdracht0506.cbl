6e65fdc556b29f2f145f442f60f28b096a6576fa
ani.createment <ani.createment@outlook.com>
2023-02-27 22:25:21 +0100

    Groffe refactoring

--- Huur een vakantieavontuur/Opdracht0506.cbl
+++ Huur een vakantieavontuur/Opdracht0506.cbl
@@ -1,77 +1,15 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. Opdracht0506.
        AUTHOR. Joey Schmitz.
-       DATE-WRITTEN. 08-02-2023 en 09-02-2023.
+       DATE-WRITTEN. 02-2023.
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
        SPECIAL-NAMES.
        Currency Sign "E" with Picture Symbol '$'.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-       SELECT KlantenBestand
-           ASSIGN TO "C:\COBOL\DATA\HUUR\Klanten.dat"
-           ORGANIZATION IS INDEXED
-           ACCESS MODE IS DYNAMIC
-           RECORD KEY IS FS-K-Klantnummer
-           FILE STATUS IS IOStatus.
-
-      *    SELECT ReserveringenBestand
-      *    ASSIGN TO "C:\COBOL\DATA\HUUR\Reserveringen.dat"
-      *    ORGANIZATION IS INDEXED
-      *    ACCESS MODE IS DYNAMIC
-      *    RECORD KEY IS FS-R-Reserveringsnummer
-      *    FILE STATUS IS IOStatus.
-
-       SELECT ReserveringenBestand
-           ASSIGN TO "C:\COBOL\DATA\HUUR\ReserveringenMetAltKey.dat"
-           ORGANIZATION IS INDEXED
-           ACCESS MODE IS DYNAMIC
-           RECORD KEY IS FS-R-Reserveringsnummer
-           ALTERNATE KEY IS FS-R-Woningnummer
-           WITH DUPLICATES
-           FILE STATUS IS IOStatus.
-
-       SELECT BewonersBestand
-           ASSIGN TO "C:\COBOL\DATA\HUUR\Bewoners.dat"
-           ORGANIZATION IS INDEXED
-           ACCESS MODE IS DYNAMIC
-           RECORD KEY IS FS-B-BewonersID
-           FILE STATUS IS IOStatus.
-       SELECT WoningenBestand
-           ASSIGN TO "C:\COBOL\DATA\HUUR\Woningen.dat"
-           ORGANIZATION IS LINE SEQUENTIAL
-           FILE STATUS IS IOStatus.
-       SELECT SysteemkengetallenBestand
-           ASSIGN TO "C:\COBOL\DATA\HUUR\Systeemkengetallen.dat"
-           ORGANIZATION IS LINE SEQUENTIAL
-           FILE STATUS IS IOStatus.
-       SELECT MutatieBestand
-           ASSIGN TO "C:\COBOL\DATA\HUUR\Mutaties.dat"
-           ORGANIZATION IS LINE SEQUENTIAL
-           FILE STATUS IS IOStatus.
 
        DATA DIVISION.
-       FILE SECTION.
-       FD KlantenBestand.
-       01 Klantrecord.
-       COPY Klant REPLACING ==(pf)== BY ==FS-K==.
-       FD ReserveringenBestand.
-       01 Reserveringsrecord.
-       COPY Reservering REPLACING ==(pf)== BY ==FS-R==.
-       FD BewonersBestand.
-       01 Bewonersrecord.
-       COPY Bewoner REPLACING ==(pf)== BY ==FS-B==.
-       FD WoningenBestand.
-       01 Woningrecord.
-         03 Woningnummer PIC 99 VALUE ZERO.
-         03 Woningtype   PIC X VALUE "S".
-       FD SysteemkengetallenBestand.
-       01 Systeemkengetallenrecord.
-         03 HoogsteKlantennummer PIC 9(8) VALUE ZERO.
-         03 HoogsteReserveringsnummer PIC 9(8) VALUE ZERO.
-       FD MutatieBestand.
-       01 Mutatierecord.
-       COPY Mutatie REPLACING ==(pf)== BY ==FS-M==.
 
        WORKING-STORAGE SECTION.
        *> Vlaggen
@@ -128,12 +66,11 @@
            05 Weken PIC X(4) VALUE "   ." OCCURS 20 TIMES.
        01 FILLER.
          03 Weekomzetten PIC 99999 VALUE ZERO OCCURS 20 TIMES.
-       01 FILLER VALUE "10S11S12L13L14S15S16S17L18L19S".
-         03 HuisjeInitieel PIC X(3) OCCURS 10.
+       
 
        *> Tellers
        01 Teller       PIC 99 VALUE ZERO.
-       01 Huisjesteller PIC 99 VALUE ZERO.
+       *>01 Huisjesteller PIC 99 VALUE ZERO.
        01 Wekenteller  PIC 99 VALUE ZERO.
        *> HumanReadable variabelen
        01 HRBedrag     PIC $ZZZ9 VALUE ZERO.
@@ -146,7 +83,6 @@
        PROCEDURE DIVISION.
        BeginProgram.
            PERFORM DisplayMenu
-           .
            STOP RUN
            .
        DisplayMenu.
@@ -235,107 +171,18 @@
            DISPLAY SPACE
            .
        Bestandsinitialisatie.
-           PERFORM VulWoningen
-           PERFORM VulSysteemkengetallen
-           PERFORM VulKlanten
-           PERFORM VulReserveringen
-           .
+           CALL "Bestandsinitialisatie".
+
+       ToevoegenReservering.
+           CALL "ToevoegenReservering".
+
+       ToevoegenReservering.
+           CALL "ToevoegenReservering".
 
        ToevoegenKlant.
-           DISPLAY SPACE
-           OPEN I-O SysteemkengetallenBestand
-           IF NOT IO-OK
-               DISPLAY ">>> Fout bij het openen van SysteemkengetallenBestand.dat: " IOStatus
-           END-IF
-           OPEN I-O KlantenBestand
-           IF NOT IO-OK
-               DISPLAY ">>> Fout bij het openen van Klanten.dat: " IOStatus
-           END-IF
-           READ SysteemkengetallenBestand
-           ADD 1               TO HoogsteKlantennummer
-           MOVE HoogsteKlantennummer
-                               TO FS-K-Klantnummer
-           DISPLAY "Achternaam: " WITH NO ADVANCING
-           ACCEPT FS-K-Naam
-           DISPLAY "Voornaam: " WITH NO ADVANCING
-           ACCEPT FS-K-Voornaam
-           DISPLAY "Straat: " WITH NO ADVANCING
-           ACCEPT FS-K-Straat
-           DISPLAY "Huisnummer: " WITH NO ADVANCING
-           ACCEPT FS-K-Huisnummer
-           DISPLAY "Postcode: " WITH NO ADVANCING
-           ACCEPT FS-K-Postcode
-           DISPLAY "Woonplaats: " WITH NO ADVANCING
-           ACCEPT FS-K-Woonplaats
-           DISPLAY "Geboortedatum: " WITH NO ADVANCING
-           ACCEPT FS-K-Geboortedatum
-           DISPLAY "Telefoonnummer: " WITH NO ADVANCING
-           ACCEPT FS-K-Telefoonnummer
-           DISPLAY "E-mail: " WITH NO ADVANCING
-           ACCEPT FS-K-Emailadres
-           WRITE Klantrecord
-           REWRITE Systeemkengetallenrecord
-           CLOSE SysteemkengetallenBestand
-           CLOSE KlantenBestand
+           CALL "ToevoegenKlant".
+           
            .
-       ToevoegenReservering.
-           DISPLAY SPACE
-           OPEN I-O SysteemkengetallenBestand
-           IF NOT IO-OK
-               DISPLAY ">>> Fout bij het openen van SysteemkengetallenBestand.dat: " IOStatus
-           END-IF
-           OPEN I-O ReserveringenBestand
-           IF NOT IO-OK
-               DISPLAY ">>> Fout bij het openen van Reserveringen.dat: " IOStatus
-           END-IF
-           READ SysteemkengetallenBestand
-           ADD 1               TO HoogsteReserveringsnummer
-           MOVE HoogsteReserveringsnummer
-                               TO FS-R-Reserveringsnummer
-           DISPLAY "Klantnummer: " WITH NO ADVANCING
-           ACCEPT FS-R-Klantnummer
-           DISPLAY "Woningnummer: " WITH NO ADVANCING
-           ACCEPT FS-R-Woningnummer
-           DISPLAY "Jaar: " WITH NO ADVANCING
-           ACCEPT FS-R-Jaar
-           IF FS-R-Jaar = ZERO
-               MOVE DatumVandaag(1:4)
-                               TO FS-R-Jaar
-           END-IF
-           DISPLAY "Weeknummer: " WITH NO ADVANCING
-           ACCEPT FS-R-Weeknummer
-           DISPLAY "Aantal weken: " WITH NO ADVANCING
-           ACCEPT FS-R-AantalWeken
-           DISPLAY "Datum creatie: " WITH NO ADVANCING
-           ACCEPT FS-R-DatumCreatie
-           IF FS-R-DatumCreatie = SPACE
-               MOVE DatumVandaag
-                               TO FS-R-DatumCreatie
-           END-IF
-           DISPLAY "Aantal bewoners: " WITH NO ADVANCING
-           ACCEPT AantalBewoners
-           PERFORM VARYING Teller FROM 1 BY 1 UNTIL Teller > AantalBewoners
-               OPEN I-O BewonersBestand
-               MOVE FS-R-Reserveringsnummer
-                               TO FS-B-Reserveringsnummer
-               MOVE Teller     TO FS-B-Volgnummer
-               DISPLAY "Initialen gast " Teller ": " WITH NO ADVANCING
-               ACCEPT FS-B-Initialen
-               DISPLAY "Geboortedatum gast " Teller ": " WITH NO ADVANCING
-               ACCEPT FS-B-Geboortedatum
-               WRITE Bewonersrecord
-               CLOSE BewonersBestand
-           END-PERFORM
-           WRITE Reserveringsrecord
-           DISPLAY ">>> Writing Reservering. IOStatus: " IOStatus
-           REWRITE Systeemkengetallenrecord
-           DISPLAY ">>> Rewriting Systeemkengetallen. IOStatus: " IOStatus
-           CLOSE SysteemkengetallenBestand
-           DISPLAY ">>> Closing SysteemkengetallenBestand. IOStatus: " IOStatus
-           CLOSE ReserveringenBestand
-           DISPLAY ">>> Closing ReserveringenBestand. IOStatus: " IOStatus
-           .
-
        BezettingsOverzicht.
            PERFORM RB90-InitProgramma
            IF NOT R-EOF
@@ -355,74 +202,8 @@
            PERFORM RB99-AfsluitenProgramma
            .
        BerekeningHuuromzet.
-           PERFORM RB90-InitProgramma
-           IF NOT R-EOF
-               PERFORM RH80-InitVerwerking
-               PERFORM UNTIL R-EOF
-                   PERFORM RB70-InitWoning
-                   PERFORM UNTIL SKM-ReserveringIngelezen NOT EQUALS SKM-ReserveringBewaard
-                       PERFORM RH60-VerwerkReservering
-                       PERFORM RB61-LeesReservering
-                   END-PERFORM
-                   PERFORM RB71-BouwWoning
-                   PERFORM RB72-SchrijfWoning
-                   PERFORM RB79-AfsluitenWoning
-               END-PERFORM
-               *> Deze routines zijn overbodig: RB81, RB82, RB89
-           END-IF
-           PERFORM RH99-AfsluitenProgramma
-           .
-       VulWoningen.
-           OPEN OUTPUT WoningenBestand
-           IF NOT IO-OK
-               DISPLAY ">>> Fout bij het openen van Woningen.dat: " IOStatus
-           END-IF
-           PERFORM VARYING Huisjesteller FROM 1 BY 1 UNTIL Huisjesteller > 10
-               MOVE HuisjeInitieel(Huisjesteller)
-                               TO Woningrecord
-               WRITE Woningrecord
-           END-PERFORM
-           CLOSE WoningenBestand
-           .
-       VulSysteemkengetallen.
-           OPEN OUTPUT SysteemkengetallenBestand
-           IF NOT IO-OK
-               DISPLAY ">>> Fout bij het openen van SysteemkengetallenBestand.dat: " IOStatus
-           END-IF
-           MOVE ZERO           TO HoogsteKlantennummer
-           MOVE ZERO           TO HoogsteReserveringsnummer
-           WRITE Systeemkengetallenrecord
-
-           CLOSE SysteemkengetallenBestand
-           .
-       VulKlanten.
-           OPEN OUTPUT KlantenBestand
-           IF NOT IO-OK
-               DISPLAY ">>> Fout bij het openen van Klanten.dat: " IOStatus
-           END-IF
-           MOVE 1              TO FS-K-Klantnummer, HoogsteKlantennummer
-           MOVE "Schmitz"      TO FS-K-Naam
-           MOVE "Joey"         TO FS-K-Voornaam
-           MOVE "Kempenaar 17" TO FS-K-Straat
-           MOVE "81"           TO FS-K-Huisnummer
-           MOVE "8231EN"       TO FS-K-Postcode
-           MOVE "Lelystad"     TO FS-K-Woonplaats
-           MOVE "19790319"     TO FS-K-Geboortedatum
-           MOVE "00310624812030"
-                               TO FS-K-Telefoonnummer
-           MOVE "4plus7@gmail.com"
-                               TO FS-K-Emailadres
-           WRITE Klantrecord
-           CLOSE KlantenBestand
-           OPEN OUTPUT SysteemkengetallenBestand
-           IF NOT IO-OK
-               DISPLAY ">>> Fout bij het openen van Systeemkengetallen.dat: " IOStatus
-           END-IF
-           MOVE 1              TO HoogsteKlantennummer
-           WRITE Systeemkengetallenrecord
-           CLOSE SysteemkengetallenBestand
-           .
-
+           CALL "BerekeningHuuromzet".
+       
        MaakMutatieBestand.
            OPEN OUTPUT MutatieBestand
            IF NOT IO-OK
@@ -430,137 +211,6 @@
            END-IF
            CLOSE MutatieBestand
            .
-       VulReserveringen.
-           OPEN I-O SysteemkengetallenBestand
-           IF NOT IO-OK
-               DISPLAY ">>> Fout bij het openen van SysteemkengetallenBestand.dat: " IOStatus
-           END-IF
-           READ SysteemkengetallenBestand
-
-           OPEN OUTPUT ReserveringenBestand
-           IF NOT IO-OK
-               DISPLAY ">>> Fout bij het openen van Reserveringen.dat: " IOStatus
-           END-IF
-           *> Woning 15, week 32, 1 week
-           MOVE "000000010000000115202332120230203"
-                               TO Reserveringsrecord
-           WRITE Reserveringsrecord
-           *> Woning 10, week 20, 3 weken
-           MOVE "000000020000000110202320320230204"
-                               TO Reserveringsrecord
-           WRITE Reserveringsrecord
-           *> Woning 17, week 18, 5 weken
-           MOVE "000000030000000217202318520230205"
-                               TO Reserveringsrecord
-           WRITE Reserveringsrecord
-           *> Woning 13, week 22, 7 weken
-           MOVE "000000040000000313202322720230206"
-                               TO Reserveringsrecord
-           WRITE Reserveringsrecord
-           *> Woning 19, week 36, 2 weken
-           MOVE "000000050000000419202336220230208"
-                               TO Reserveringsrecord
-           WRITE Reserveringsrecord
-           MOVE 5              TO HoogsteReserveringsnummer
-           REWRITE Systeemkengetallenrecord
-           CLOSE SysteemkengetallenBestand
-           CLOSE ReserveringenBestand
-           .
-       RB90-InitProgramma.
-           SET R-NotEOF        TO TRUE
-           OPEN INPUT ReserveringenBestand
-           MOVE ZERO           TO FS-R-Woningnummer
-           START ReserveringenBestand
-             KEY > FS-R-Woningnummer
-           END-START
-           PERFORM RB61-LeesReservering
-           .
-       RB99-AfsluitenProgramma.
-           PERFORM RenderBezettingstabel
-           CLOSE ReserveringenBestand
-           .
-       RB80-InitVerwerking.
-           MOVE SKM-ReserveringIngelezen
-                               TO SKM-ReserveringBewaard
-           *> Alle weekomzetten weer op nul zetten
-           PERFORM VARYING Teller FROM 1 BY 1 UNTIL Teller > 20
-               MOVE ZERO       TO Weekomzetten(Teller)
-           END-PERFORM
-           .
-       RB70-InitWoning.
-           MOVE SKM-ReserveringIngelezen
-                               TO SKM-ReserveringBewaard
-           .
-       RB71-BouwWoning.
-           .
-
-       RB72-SchrijfWoning.
-           .
-       RB79-AfsluitenWoning.
-           .
-       RB60-VerwerkReservering.
-           *> Dit is de laatste subroutine waar we directe toegang hebben tot een gelezen record.
-           *> Daarom vind hier de verwerking plaats. In dit geval: het plaatsen van de juiste letters R.
-           *> Van belang zijn de velden FS-R-Woningnummer (voor de rij), FS-R-JaarWeek (de kolom),
-           *> en FS-R-AantalWeken (voor de iteratie).
-           IF (FS-R-Jaar EQUALS 2023)
-               PERFORM VARYING Teller FROM 1 BY 1 UNTIL Teller > FS-R-AantalWeken
-                   *> Onderscheid maken tussen boeking, reservering en verlopen
-                   MOVE "   R" TO Weken(FS-R-Woningnummer - 9, FS-R-Weeknummer - 18 + Teller)
-                   IF FS-R-DatumBetaling > "        "
-                       MOVE "   B"
-                               TO Weken(FS-R-Woningnummer - 9, FS-R-Weeknummer - 18 + Teller)
-                   END-IF
-
-                   IF FS-R-DatumVerlopen > "        "
-                       MOVE "   ."
-                               TO Weken(FS-R-Woningnummer - 9, FS-R-Weeknummer - 18 + Teller)
-                   END-IF
-                   IF FS-R-DatumAnnulering > "        "
-                       MOVE "   ."
-                               TO Weken(FS-R-Woningnummer - 9, FS-R-Weeknummer - 18 + Teller)
-                   END-IF
-               END-PERFORM
-           END-IF
-           .
-       RB61-LeesReservering.
-           MOVE SKM-ReserveringIngelezen
-                               TO SKM-ReserveringBewaard
-           READ ReserveringenBestand NEXT RECORD
-               AT END
-                   SET R-EOF   TO TRUE
-               NOT AT END
-                   MOVE FS-R-Woningnummer
-                               TO SKM-R-N-Woningnummer
-                   DISPLAY "Read gedaan op huisje " FS-R-Woningnummer
-                   MOVE FS-R-JaarWeek
-                               TO SKM-R-N-Jaarweek
-           END-READ
-           .
-
-       *> SUBROUTINES MET EEN AANGEPASTE INHOUD TBV BEREKENING HUUROMZETTEN
-       RH99-AfsluitenProgramma.
-           PERFORM RenderOmzettentabel
-           CLOSE ReserveringenBestand
-           .
-       RH80-InitVerwerking.
-           MOVE SKM-ReserveringIngelezen
-                               TO SKM-ReserveringBewaard
-           *> Alle weekomzetten weer op nul zetten
-           PERFORM VARYING Teller FROM 1 BY 1 UNTIL Teller > 20
-               MOVE ZERO       TO Weekomzetten(Teller)
-           END-PERFORM
-           .
-       RH60-VerwerkReservering.
-           *> Dit is de laatste subroutine waar we directe toegang hebben tot een gelezen record.
-           *> Daarom vind hier de verwerking plaats. In dit geval: het ophogen van de weekomzet.
-           IF (FS-R-Jaar EQUALS 2023)
-               PERFORM VARYING Teller FROM 0 BY 1 UNTIL Teller EQUALS FS-R-AantalWeken
-                   ADD 10      TO Weekomzetten(FS-R-Weeknummer - 17 + Teller)
-               END-PERFORM
-           END-IF
-           .
-
        AnnuleerBoeking.
            .
        AnnuleerReservering.
