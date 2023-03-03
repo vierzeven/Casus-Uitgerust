792e63b7099da60c5b8b15308fce25f4677bfc87
ani.createment <ani.createment@outlook.com>
2023-03-03 10:07:14 +0100

    display leeftijd

--- Huur een vakantieavontuur/TonenToeristenBelasting.cbl
+++ Huur een vakantieavontuur/TonenToeristenBelasting.cbl
@@ -25,6 +25,10 @@
            RECORD KEY IS FS-B-BewonersID
            FILE STATUS IS IOStatus.
 
+       SELECT DatumWeekBestand ASSIGN TO "C:\COBOL\DATA\HUUR\DatumWeek.dat"
+           ORGANIZATION IS LINE SEQUENTIAL
+           FILE STATUS IS IOStatus.
+
        DATA DIVISION.
        FILE SECTION.
        FD ReserveringenBestand.
@@ -33,24 +37,41 @@
        FD BewonersBestand.
        01 Bewonersrecord.
        COPY Bewoner REPLACING ==(pf)== BY ==FS-B==.
+       FD DatumWeekBestand.
+       01 DatumWeekRecord.
+         03 WeekNummer PIC 99.
+         03 WeekDatum PIC 9(8).
 
        WORKING-STORAGE SECTION.
        01 IOStatus PIC 99 VALUE ZERO.
          88 IO-OK VALUE ZERO.
-       01 EOF PIC 99 VALUE 0.
-         88 NotEOF VALUE 1.
-       
-       01 Maand PIC 9(2).
-       01 BeginDatumMaand PIC 9(8).
-       01 EindDatumMaand PIC 9(8).
+       01 EOFReserveringenVlag PIC 99 VALUE 0.
+         88 EOFReserveringen VALUE 0.
+         88 NotEOFReserveringen VALUE 1.
+       01 EOFBewonersVlag PIC 99 VALUE 0.
+         88 EOFBewoners VALUE 0.
+         88 NotEOFBewoners VALUE 1.
+       01 EOFDatumWeekVlag PIC 99 VALUE 0.
+         88 EOFDatumWeek VALUE 0.
+         88 NotEOFDatumWeek VALUE 1.
+
+       01 Week PIC 9(2).
+       01 BeginDatumWeek PIC 9(8).
+       01 EindDatumWeek PIC 9(8).
+       01 WS-WeekDatum PIC 9(8).
+       01 WS-INT-WeekDatum PIC 9(8).
 
        01 BeginDatumReservering PIC 9(8).
        01 EindDatumReservering PIC 9(8).
 
+       01 WS-Geboortedatum PIC 9(8).
+       01 WS-INT-Geboortedatum PIC 9(8).
+       01 WS-INT-Leeftijd PIC 999.
+
        PROCEDURE DIVISION.
 
-           DISPLAY "Over welke maand wil je de toeristen belasting zien (geef maandnummer): " WITH NO ADVANCING
-           ACCEPT Maand
+           DISPLAY "Voor welke week wil je de toeristen belasting zien (geef weeknummer): " WITH NO ADVANCING
+           ACCEPT Week
 
            *>COMPUTE FUNCTION INTEGER-OF-DATE(BeginDatumMaand) FUNCTION INTEGER-OF-DATE(EindDatumMaand)
 
@@ -66,13 +87,43 @@
            END-IF
 
            READ ReserveringenBestand NEXT RECORD
-
-           *>Vergelijking reservering met begin en einddatum maand
-
                AT END
-                   SET EOF TO 0
-           END-READ.
+                   SET EOFReserveringen TO TRUE
+           END-READ
 
+           PERFORM UNTIL EOFReserveringen
+               IF (Week EQUALS FS-R-Weeknummer)
+
+                   READ BewonersBestand NEXT RECORD
+                       AT END
+                           SET EOFBewoners TO TRUE
+                   END-READ
+
+                   PERFORM UNTIL EOFBewoners
+                       IF (FS-B-Reserveringsnummer EQUALS FS-R-Reserveringsnummer)
+                           MOVE FS-B-Geboortedatum TO WS-Geboortedatum
+                             MOVE FUNCTION INTEGER-OF-DATE(WS-Geboortedatum) TO WS-INT-Geboortedatum
+
+                             OPEN INPUT DatumWeekBestand 
+                             READ DatumWeekBestand 
+                                 AT END
+                                 SET EOFDatumWeek TO TRUE 
+                             END-READ 
+
+                             IF FS-R-Weeknummer EQUALS WeekNummer
+                                 MOVE WeekDatum TO WS-WeekDatum
+                                 MOVE FUNCTION INTEGER-OF-DATE(WS-WeekDatum) TO WS-INT-WeekDatum
+
+                                 COMPUTE WS-INT-Leeftijd EQUALS (WS-INT-WeekDatum - WS-INT-Geboortedatum)/365.25
+                                 DISPLAY "Leeftijd is: " WS-INT-Leeftijd
+                             END-IF
+                       END-IF
+                   END-PERFORM
+
+               END-IF
+           END-PERFORM
+           *>Vergelijking reservering met begin en einddatum maand
+           *> Vergeet niet te sluiten
            
 
 
