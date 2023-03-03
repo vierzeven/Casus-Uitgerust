2166ecc8f72ab638c13e6f823c0430b5af094ed5
Ming Geng <ming.createment@outlook.com>
2023-03-03 13:52:09 +0100

    Read Indexed bestand aangepast. Key in variabele + MOVE

---
+++ Huur een vakantieavontuur/test summary reports/2. Wijzigen klantgegevens.cbl
@@ -0,0 +1,48 @@
+       *> TEST SUMMARY REPORT VOOR wijzigen klantgegevens
+           
+
+       *> 1. validatie klant opzet.
+          *> Klantnummer wordt gezocht in het klantenbestand. Om de gegevens te wijzigen zal de klant met een klantnummer in het klantenbestand
+          *> voor moeten komen. Indien het klantnummer niet voorkomt zal " Klantnummer bestaat niet" getoond worden.
+
+          *> Test Uitgevoerd: 
+          *> - Er staat in de initialisatie maar 1 klant in het klantenbestand. 
+          *> - Bij invoering klantnr. 1 zal het klantrecord getoond worden.
+          *> - Bij invoering <> 1 wordt  " Klantnummer bestaat niet"  getoond.
+
+          *> TEST GESLAAGD.
+
+
+
+       *> 2. DISPLAY Gegevens
+        *> Het klantrecord wordt op je juiste manier getoond. Er staat in het klantenbestand 1 record met inhoud:
+      *Klantnummer: 00000001
+      *Klantnaam: Joey Schmitz
+      *Adres: Kempenaar 1781   , 8231EN Lelystad
+      *Geboortedatum: 19-03-1979
+      *Telefoonnummer: 00310624812030
+      *E-mail: 4plus7@gmail.com
+
+        
+        *> Test Uitgevoerd.
+        *> - Bij het invoeren van 1 toont het programma:
+      *Klantnummer: 00000001
+      *Klantnaam: Joey Schmitz
+      *Adres: Kempenaar 1781   , 8231EN Lelystad
+      *Geboortedatum: 19-03-1979
+      *Telefoonnummer: 00310624812030
+      *E-mail: 4plus7@gmail.com
+
+
+
+      *> TEST GESLAAGD
+
+       *> 3. Bij het veranderen van Telefoonnummer wordt in het klantenbestand het juiste element veranderd. Hierbij kan de invoer alfanumeriek zijn.
+       
+       *> Test uitgevoerd.
+       *> test geslaagd
+
+       *> 3. Bij het veranderen van Emailadres wordt in het klantenbestand het juiste element veranderd. Hierbij kan de invoer alfanumeriek zijn.
+        
+       *> Test uitgevoerd.
+       *> test geslaagd.
